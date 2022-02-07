d6f37ce745c943d2f515ddfeb5d49423e4dd7f28
Ritika Rane <97068483+raneritika@users.noreply.github.com>
2022-02-06 17:09:31 -0800

    B5 (#3)
    * Update app.js
    
    * Update swagger (1).yaml

--- app.js
+++ app.js
@@ -16,12 +16,22 @@ app.post('/v1/auth',post_auth)
 
 function get_weather_v1(request, response)
 {
-     response.json({"coord":{"lon":-123.262,"lat":44.5646},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01n"}],"base":"stations","main":{"temp":282.61,"feels_like":282.61,"temp_min":280.58,"temp_max":285.29,"pressure":1018,"humidity":84},"visibility":10000,"wind":{"speed":0.89,"deg":225,"gust":0.89},"clouds":{"all":0},"dt":1642038331,"sys":{"type":2,"id":2040223,"country":"US","sunrise":1642002454,"sunset":1642035291},"timezone":-28800,"id":5720727,"name":"Corvallis","cod":200})
+     let tokenn= req.query.tokenn
+        //let d1=request.body.expires
+        if(token.includes(tokenn))
+        {
+          response.json({"coord":{"lon":-123.262,"lat":44.5646},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01n"}],"base":"stations","main":{"temp":282.61,"feels_like":282.61,"temp_min":280.58,"temp_max":285.29,"pressure":1018,"humidity":84},"visibility":10000,"wind":{"speed":0.89,"deg":225,"gust":0.89},"clouds":{"all":0},"dt":1642038331,"sys":{"type":2,"id":2040223,"country":"US","sunrise":1642002454,"sunset":1642035291},"timezone":-28800,"id":5720727,"name":"Corvallis","cod":200})
+        }
 }
 
 function get_hello(req,res)
 {
+     let tokenn= req.query.tokenn
+        //let d1=request.body.expires
+        if(token.includes(tokenn))
+        {
     res.json({"Heya": " How you Doing?"})
+        }
 }
 
 function post_auth(req,res)
